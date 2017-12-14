da4d827cfa5b28cb68afd8a1c531a5d6d66a8f06
chiennd <chiennd@vng.com.vn>
2017-12-14 18:52:15 +0700

    add image detection

--- src/main/java/com/zalo/hackathon/controller/HackathonController.java
+++ src/main/java/com/zalo/hackathon/controller/HackathonController.java
@@ -1,16 +1,35 @@
 package com.zalo.hackathon.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.zalo.hackathon.utils.LogCenter;
+import okhttp3.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import java.util.Arrays;
 
 @Path("/")
 public class HackathonController {
     private static Logger LOG = LogManager.getLogger(HackathonController.class);
+    private OkHttpClient client = new OkHttpClient();
+
+    private static final String URL_POST_IMAGE = "http://45.124.94.45:8080/predict";
+    private static MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+//    public HackathonController() {
+//        client = new OkHttpClient();
+//    }
+
+    @Path("/test")
+    @GET
+    public String test() {
+        return "Success";
+    }
 
     @GET
     @Path("/")
@@ -25,9 +44,52 @@ public class HackathonController {
                         @QueryParam("timestamp") String timestamp,
                         @QueryParam("mac") String mac) {
 
-
-        LogCenter.info(LOG, "Receive message" + message);
-
-        return "Receive message " + message;
+        LogCenter.info(LOG, "Receive " + message);
+        return message;
+//        switch (event) {
+//            case "sendimagemsg":
+//                LogCenter.info(LOG, "Receive image message: " + href);
+//                JSONObject result = processSendImage(href);
+//
+//                if (result.getBoolean("error")) {
+//                    return "Sorry, Mình không thể nhận diện ảnh này";
+//                } else {
+//                    return result.getString("message");
+//                }
+//        }
+//
+//        return "Hello World";
     }
+
+    private JSONObject processSendImage(String href) {
+        JSONObject object = new JSONObject()
+                .put("service", "clothing")
+                .put("data", Arrays.asList(href))
+                .put("parameters", new JSONObject()
+                        .put("output",
+                                new JSONObject().put("best", 5))
+                );
+
+        Request request = new Request.Builder()
+                .url(URL_POST_IMAGE)
+                .post(RequestBody.create(JSON, object.toString()))
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String body = response.body().string();
+
+            return new JSONObject().put("error", false).put("message", body);
+        } catch (Exception e) {
+            return new JSONObject().put("error", true).put("message", e.getMessage());
+        }
+    }
+
+    public static void main(String args[]) {
+        HackathonController controller = new HackathonController();
+        JSONObject result = controller.processSendImage("http://maysonghanh.com/wp-content/uploads/sites/820/2017/03/áo-phông-cổ-dệt-màu-xanh-bích.jpg");
+
+        System.out.println(result);
+    }
+
 }
